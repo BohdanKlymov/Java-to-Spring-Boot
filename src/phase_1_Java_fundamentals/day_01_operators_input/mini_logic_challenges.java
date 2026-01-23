a9606af09104a1796ff6852850972b600305c905
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2026-01-23 16:11:59 +0100

    Added first and second practice exercises of day 1. Started with challenge 1/3.

---
+++ src/phase_1_Java_fundamentals/day_01_operators_input/mini_logic_challenges.java
@@ -0,0 +1,16 @@
+package phase_1_Java_fundamentals.day_01_operators_input;
+import java.util.Scanner;
+
+public class mini_logic_challenges {
+    public static void main(String[] args) {
+
+//        Challenge 1 - Even or Odd
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println();
+    }
+}
---
+++ src/phase_1_Java_fundamentals/day_01_operators_input/practice_exercise_2.java
@@ -0,0 +1,26 @@
+package phase_1_Java_fundamentals.day_01_operators_input;
+import java.util.Scanner;
+
+public class practice_exercise_2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your height: ");
+        double height = scanner.nextDouble();
+
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+        scanner.close();
+    }
+}
