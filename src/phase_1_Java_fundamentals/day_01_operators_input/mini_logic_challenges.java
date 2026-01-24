f2eaa376d6d4eb974f7727670c9143d52d2bcef4
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2026-01-24 09:08:23 +0100

    Finished mini logic challenges.

--- src/phase_1_Java_fundamentals/day_01_operators_input/mini_logic_challenges.java
+++ src/phase_1_Java_fundamentals/day_01_operators_input/mini_logic_challenges.java
@@ -11,6 +11,35 @@ public class mini_logic_challenges {
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        System.out.println();
+
+        if (number % 2 == 0){
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+
+//        Challenge 2 - Simple Grade Checker
+
+        System.out.print("Enter a number between 0 and 100: ");
+        int number1 = scanner.nextInt();
+
+        if (number >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+//        Challenge 3 - Age Category
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age < 13) {
+            System.out.println("Child");
+        } else if (age < 20) {
+            System.out.println("Teen");
+        } else {
+            System.out.println("Adult");
+        }
     }
 }
