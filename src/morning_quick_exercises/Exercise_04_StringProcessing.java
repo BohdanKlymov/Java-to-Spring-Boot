7002fb2f290cf9daf389e62b298a0b8356fcf3b6
Bohdan Klymov <programmer.b.k.339@gmail.com>
2026-02-24 09:12:17 +0100

    Finished exercise 4 of morning routine.

--- src/morning_quick_exercises/Exercise_04_StringProcessing.java
+++ src/morning_quick_exercises/Exercise_04_StringProcessing.java
@@ -6,8 +6,9 @@ public class Exercise_04_StringProcessing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String cV = convertTOlowercase(sc);
-        System.out.println(cV);
+        String input = convertTOlowercase(sc);
+
+        System.out.println(countVowels(input));
 
         sc.close();
     }
@@ -15,10 +16,35 @@ public class Exercise_04_StringProcessing {
 
     public static String convertTOlowercase(Scanner sc) {
 
+        System.out.print("Write a random Text: ");
         String input = sc.nextLine();
 
         input = input.toLowerCase(Locale.ROOT);
 
         return input;
+    }
+
+    public static int countVowels(String input) {
+        if (input == null) {
+            return 0;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = Character.toLowerCase(input.charAt(i));
+
+            switch (ch) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    count++;
+                    break;
+            }
+        }
+
+        return count;
     }
 }
