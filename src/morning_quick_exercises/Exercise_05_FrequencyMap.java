8d85e3d7cee1dfc086b47b1ba4a993c92b203b6c
Bohdan Klymov <programmer.b.k.339@gmail.com>
2026-03-25 09:36:03 +0100

    Have finished morning exercise 5.

--- src/morning_quick_exercises/Exercise_05_FrequencyMap.java
+++ src/morning_quick_exercises/Exercise_05_FrequencyMap.java
@@ -2,26 +2,36 @@ package morning_quick_exercises;
 import java.util.*;
 
 public class Exercise_05_FrequencyMap {
-    public static void main(System[] args) {
-        ArrayList<String> fruits = new ArrayList<>(List.of("apple", "banana", "apple", "orange", "banana", "apple"));
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>(
+                List.of("apple", "banana", "apple", "orange", "banana", "apple")
+        );
 
-//        System.out.println(countOccurrences(fruits));
+        System.out.println(countOccurrences(fruits));
 
-//        Map<String, Integer> sortedFruits = countOccurrences(fruits);
-//
-//        for (Map.Entry<String, Integer> entry : sortedFruits.entrySet()) {
-//            System.out.println(entry.getKey() + " = " + entry.getValue());
-//        }
+        Map<String, Integer> sortedFruits = countOccurrences(fruits);
+
+        for (Map.Entry<String, Integer> entry : sortedFruits.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
     }
 
+    public static LinkedHashMap<String, Integer> countOccurrences(List<String> fruits) {
+        LinkedHashMap<String, Integer> sortedFruits = new LinkedHashMap<>();
+        HashMap<String, Integer> frequencyMap = new HashMap<>();
 
-//    public static LinkedHashMap<String, Integer> countOccurrences(List<String> fruits) {
-//
-//        LinkedHashMap<String, Integer> sortedFruits = new LinkedHashMap<>();
-//
-//        for (int i = 0; i < fruits.size(); i++) {
-//
-//        }
-//
-//    }
+        for (int i = 0; i < fruits.size(); i++) {
+            String fruit = fruits.get(i);
+            frequencyMap.put(fruit, frequencyMap.getOrDefault(fruit, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
+        entryList.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+        for (Map.Entry<String, Integer> entry : entryList) {
+            sortedFruits.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedFruits;
+    }
 }
